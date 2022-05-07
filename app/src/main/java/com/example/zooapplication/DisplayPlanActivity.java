75d286ef9f57d8b168ab3bb9e6c4f286634ba94d
elielgranados <eliel.granados@outlook.com>
2022-05-06 17:43:47 -0700

    Deleted PlanList Files

--- app/src/main/java/com/example/zooapplication/DisplayPlanActivity.java
+++ app/src/main/java/com/example/zooapplication/DisplayPlanActivity.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 public class DisplayPlanActivity extends AppCompatActivity {
     public RecyclerView recyclerView;
-    private PlanListViewModel viewModel;
     private ArrayList<String> display;
 
     @Override
@@ -21,19 +20,6 @@ public class DisplayPlanActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_plan);
         display = getIntent().getStringArrayListExtra("result");
-        PlanListViewModel viewModel = new ViewModelProvider(this)
-                .get(PlanListViewModel.class);
-
-        PlanListItemDao todoListItemDao = PlanDatabase.getSingleton(this).planListItemDao();
-        List<PlanListItems> todoListItems = todoListItemDao.getAll();
-
-        PlanListAdapter adapter = new PlanListAdapter();
-        adapter.setHasStableIds(true);
-        adapter.setPlanListItems(todoListItems);
-
-        recyclerView = findViewById(R.id.plan_items);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
 
 //
 //        super.onCreate(savedInstanceState);
