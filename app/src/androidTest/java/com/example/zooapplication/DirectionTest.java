2ddf040088199b82d3795c145991479ab0a47f97
chen jianzhang <chenjianzhang@chens-MacBook-Air.local>
2022-05-23 22:54:08 -0700

    update UI test after changing to recyclerView

--- app/src/androidTest/java/com/example/zooapplication/DirectionTest.java
+++ app/src/androidTest/java/com/example/zooapplication/DirectionTest.java
@@ -1,105 +1,99 @@
-//package com.example.zooapplication;
-//
-//
-//import androidx.test.espresso.DataInteraction;
-//import androidx.test.espresso.Espresso;
-//import androidx.test.espresso.ViewInteraction;
-//import androidx.test.espresso.action.ViewActions;
-//import androidx.test.filters.LargeTest;
-//import androidx.test.rule.ActivityTestRule;
-//import androidx.test.runner.AndroidJUnit4;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.view.ViewParent;
-//
-//import static androidx.test.InstrumentationRegistry.getInstrumentation;
-//import static androidx.test.espresso.Espresso.onData;
-//import static androidx.test.espresso.Espresso.onView;
-//import static androidx.test.espresso.Espresso.pressBack;
-//import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-//import static androidx.test.espresso.action.ViewActions.*;
-//import static androidx.test.espresso.assertion.ViewAssertions.*;
-//import static androidx.test.espresso.matcher.RootMatchers.isPlatformPopup;
-//import static androidx.test.espresso.matcher.ViewMatchers.*;
-//
-//import com.example.zooapplication.R;
-//
-//import org.hamcrest.Description;
-//import org.hamcrest.Matcher;
-//import org.hamcrest.TypeSafeMatcher;
-//import org.hamcrest.core.IsInstanceOf;
-//import org.junit.Rule;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//
-//import static org.hamcrest.CoreMatchers.containsString;
-//import static org.hamcrest.CoreMatchers.instanceOf;
-//import static org.hamcrest.Matchers.allOf;
-//import static org.hamcrest.Matchers.anything;
-//import static org.hamcrest.Matchers.is;
-//import static java.lang.Thread.sleep;
-//
-//@LargeTest
-//@RunWith(AndroidJUnit4.class)
-///**
-// * Class name: DirectionTest
-// * Description: Test if the direction can be displayed correctly
-// * public function:
-// *      DirectionTest - Check if the plan and route information can be displayed
-// *
-// * */
-//public class DirectionTest {
-//
-//    @Rule
-//    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
-//
-//    @Test
-//    /**
-//     * Test the if the direction display correctly
-//     * */
-//    public void PlanTest() throws InterruptedException {
-//
-//        // Add exhibits to the list
-//        onView(withId(R.id.search_bar)).perform(typeText("el"));
-//        sleep(500);
-//        onView(withText(containsString("elephant odyssey"))).inRoot(isPlatformPopup()).perform(click());
-//        sleep(500);
-//        onView(withId(R.id.search_bar)).perform(typeText("li"));
-//        sleep(500);
-//        onView(withText(containsString("lions"))).inRoot(isPlatformPopup()).perform(click());
-//        sleep(500);
-//        onView(withId(R.id.search_bar)).perform(typeText("f"));
-//        sleep(500);
-//        onView(withText(containsString("arctic fox"))).inRoot(isPlatformPopup()).perform(click());
-//        sleep(500);
-//        Espresso.closeSoftKeyboard();
-//
-//        // Click the "PLAN" button
-//        sleep(500);
-//        onView(withId(R.id.button11)).perform(click());
-//
-//        // Check if the context of the list is correct
-//        String[] context = new String[]{"lions", "elephant_odyssey", "arctic_foxes"}; // a string list contains the excepted output value
-//        for (int idx = 0; idx <context.length; idx++) {
-//            onData(anything())
-//                    .inAdapterView(withId(R.id.planlist))
-//                    .atPosition(idx)
-//                    .check(matches(withText(context[idx])));
-//        }
-//
-//        // Click "Direction" button
-//        sleep(500);
-//        onView(withId(R.id.direction)).perform(click());
-//        // Click the "NEXT" button
-//        sleep(500);
-//        onView(withId(R.id.getNextDirection)).perform(click());
-//        sleep(500);
-//        onView(withId(R.id.getNextDirection)).perform(click());
-//        sleep(500);
-//        onView(withId(R.id.getNextDirection)).perform(click());
-//
-//        // Check if Error message display when the trip is finished
-//        onView(withText("The route is done!")).check(matches(isDisplayed()));
-//
-//    }
-//}
+package com.example.zooapplication;
+
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.isPlatformPopup;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.Matchers.anything;
+import static java.lang.Thread.sleep;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+/**
+ * Class name: DirectionTest
+ * Description: Test if the direction can be displayed correctly
+ * public function:
+ *      DirectionTest - Check if the plan and route information can be displayed
+ *
+ * */
+public class DirectionTest {
+
+    @Rule
+    public ActivityTestRule<ExhibitsActivity> mActivityTestRule = new ActivityTestRule<>(ExhibitsActivity.class);
+
+    @Test
+    /**
+     * Test the if the direction display correctly
+     * */
+    public void PlanTest() throws InterruptedException {
+
+        // clear the recyclerView
+        sleep(500);
+        onView(withId(R.id.clear_all)).perform(click());
+        sleep(500);
+        // Add exhibits to the list
+        onView(withId(R.id.search_bar)).perform(typeText("el"));
+        sleep(500);
+        onView(withText(containsString("elephant"))).inRoot(isPlatformPopup()).perform(click());
+        sleep(500);
+        onView(withId(R.id.search_bar)).perform(typeText("li"));
+        sleep(500);
+        onView(withText(containsString("lions"))).inRoot(isPlatformPopup()).perform(click());
+        sleep(500);
+        onView(withId(R.id.search_bar)).perform(typeText("f"));
+        sleep(500);
+        onView(withText(containsString("fox"))).inRoot(isPlatformPopup()).perform(click());
+        sleep(500);
+        Espresso.closeSoftKeyboard();
+
+        // Check if the context of the list(recyclerView) is correct
+        String[] context = new String[]{"Elephant Odyssey", "Lions", "Arctic Foxes"}; // a string list contains the excepted output value
+        for (int idx = 0; idx <context.length; idx++) {
+            // try ti scroll to an item that contains the exhibit
+            onView(ViewMatchers.withId(R.id.dis))
+                    // scroll with fail if item is not on the recyclerView
+                    .perform(RecyclerViewActions.scrollTo(
+                            hasDescendant(withText(context[idx])))
+                    );
+        }
+
+        // Click the "PLAN" button
+        sleep(500);
+        onView(withId(R.id.button11)).perform(click());
+
+
+        // Click "Direction" button
+        sleep(500);
+        onView(withId(R.id.direction)).perform(click());
+        // Click the "NEXT" button
+        sleep(500);
+        onView(withId(R.id.getNextDirection)).perform(click());
+        sleep(500);
+        onView(withId(R.id.getNextDirection)).perform(click());
+        sleep(500);
+        onView(withId(R.id.getNextDirection)).perform(click());
+
+        // Check if Error message display when the trip is finished
+        onView(withText("The route is done!")).check(matches(isDisplayed()));
+
+    }
+}
